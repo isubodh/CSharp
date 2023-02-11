855e0195812e18c232a38d35dca64e96d0c7be1b
Subodh Sharma <isubodh@gmail.com>
2023-02-11 09:30:46 +0530

    .

--- Composite/Program.cs
+++ Composite/Program.cs
@@ -15,6 +15,10 @@ l1.Add(new FileObj("one.txt", 100));
 l1.Add(new DirectoryObj("d1", 10));
 l1.Add(new DirectoryObj("d2", 10));
 
+var l2 = new DirectoryObj("level3", 50);
+l2.Add(new FileObj("one.txt", 100));
+root.Add(l2);
+
 Console.WriteLine($"root dir before : {l1.Name} : {l1.GetSize()}");
 
 root.Add(l1);
